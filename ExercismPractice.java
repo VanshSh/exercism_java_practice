b72e6d0f5a2fdcf682bea28433d5e6d31f004c00
VanshSh <124032030+VanshSharmaS7C@users.noreply.github.com>
2024-01-11 23:04:12 +0530

    Revert "feat: :sparkles: Lesson: Leap"
    This reverts commit 64d259aa4eec2f8df710f685d5b7465ac4cf1f36.

--- ExercismPractice.java
+++ ExercismPractice.java
@@ -1,26 +1,43 @@
-class Leap {
+import java.util.HashMap;
+import java.util.Map;
 
-    boolean isLeapYear(int year) {
-        boolean divisibleBy4 = year % 4 == 0;
-        boolean divisibleBy100 = year % 100 == 0;
-        boolean divisibleBy400 = year % 400 == 0;
+class RnaTranscription {
+    private final Map<Character, Character> transcriptionMap;
 
-        if (divisibleBy4 && (!divisibleBy100 || divisibleBy400)) {
-            return true;
-        } else {
-            return false;
+    public RnaTranscription() {
+        transcriptionMap = new HashMap<>();
+        transcriptionMap.put('G', 'C');
+        transcriptionMap.put('C', 'G');
+        transcriptionMap.put('T', 'A');
+        transcriptionMap.put('A', 'U');
+    }
+
+    String transcribe(String dnaStrand) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : dnaStrand.toCharArray()) {
+            if (transcriptionMap.containsKey(c)) {
+
+                sb.append(transcriptionMap.get(c));
+
+            } else {
+                throw new IllegalArgumentException("Invalid nucleotides: " + sb.toString().trim());
+
+            }
         }
+        return sb.toString();
     }
 
 }
 
 public class ExercismPractice {
     public static void main(String[] args) {
-        Leap testYear = new Leap();
-        System.out.println(testYear.isLeapYear(2015));
-        System.out.println(testYear.isLeapYear(1970));
-        System.out.println(testYear.isLeapYear(1996));
-        System.out.println(testYear.isLeapYear(1960));
+        RnaTranscription transcriber = new RnaTranscription();
 
+        // Example usage:
+        String dnaStrand = "T";
+        String rnaStrand = transcriber.transcribe(dnaStrand);
+
+        System.out.println("DNA Strand: " + dnaStrand);
+        System.out.println("RNA Strand: " + rnaStrand);
     }
 }
